6543c08936d85f0ce97b143fc408a4986be49cc5
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-07-28 00:28:11 +0600

    feature - Build user model

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const authSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = mongoose.model('Users', authSchema);
+
+export default User;
