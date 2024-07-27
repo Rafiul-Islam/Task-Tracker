f5b6139f881c9e000557159e90d4ca6e649ba217
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-07-28 00:51:37 +0600

    feature - save the user password in encrypted form

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export async function signup(req, res) {
   const body = req.body;
@@ -9,8 +10,14 @@ export async function signup(req, res) {
   try {
     const {name, email, password} = body;
     const user = await User.findOne({email});
+
     if (user) return res.status(400).send({message: 'User already exists'});
-    const savedUser = await User.create({name, email, password});
+
+    const saltRounds = 10;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+    const savedUser = await User.create({name, email, password: encryptedPassword});
+
     return res.status(201).send(savedUser);
   } catch (error) {
     return res.status(500).send({message: 'Something went wrong'});
